be793930413dfda66e3619b2bec215860a0391f8
michael-mbugua <kamirimichael369@gmail.com>
2023-06-10 17:31:15 +0300

    import component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Homepage from "./components/homepage/Homepage";
+import Product from "./components/product/Product";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Homepage />
+      {/* <Product /> */}
     </div>
   );
 }
