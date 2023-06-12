131cfaa501ff97a38d76d72aaa8485790ff6a596
michael-mbugua <kamirimichael369@gmail.com>
2023-06-12 13:04:32 +0300

    import pagination component

--- src/components/sneakers/Sneakers.jsx
+++ src/components/sneakers/Sneakers.jsx
@@ -1,26 +1,27 @@
 import "./sneakers.css";
 import React, { useState, useEffect } from "react";
 import Card from "../card/Card";
+import Pagination from "../pagination/Pagination";
 
-function Sneakers({ data }) {
+function Sneakers() {
   const [clothes, setClothes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
     setIsLoading(true);
 
-    fetch("https://quinafashion.onrender.com/clothings").then((resp) =>
-      resp
-        .json()
-        .then((data) => {
-          setClothes(data);
-          setIsLoading(false);
-        })
-        .catch((err) => {
-          console.log(err);
-          setIsLoading(false);
-        })
-    );
+    fetch("https://quinafashion.onrender.com/clothings")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setClothes(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
@@ -31,39 +32,40 @@ function Sneakers({ data }) {
     );
   }
 
-  let cloth = clothes.map((e) => {
-    return (
-      <Card
-        key={e.id}
-        type={e.weartype}
-        amount={e.price}
-        image={e.image}
-        size={e.size}
-      />
-    );
-  });
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = clothes.slice(indexOfFirstPost, indexOfLastPost);
+
+  const renderCards = currentPosts.map((e) => (
+    <Card
+      key={e.id}
+      type={e.weartype}
+      amount={e.price}
+      image={e.image}
+      size={e.size}
+    />
+  ));
+
+  const handlePaginationClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
-    <div className="sneaker__container">
-      <div className="sneaker__header">
-        <h2>Available Fashions</h2>
-      </div>
+    <div className="fashion__container">
+      <div className="sneaker__container">
+        <div className="sneaker__header">
+          <h2>Available Fashions</h2>
+        </div>
 
-      <div className="sneaer-card-container">
-        {data
-          ? data &&
-            data.map((e) => {
-              return (
-                <Card
-                  key={e.id}
-                  type={e.weartype}
-                  amount={e.price}
-                  image={e.image}
-                  size={e.size}
-                />
-              );
-            })
-          : cloth}
+        <div className="sneaer-card-container">{renderCards}</div>
+      </div>
+      <div className="paginate">
+        <Pagination
+          postsPerPage={postsPerPage}
+          totalPosts={clothes.length}
+          onPageClick={handlePaginationClick}
+          currentPage={currentPage}
+        />
       </div>
     </div>
   );
