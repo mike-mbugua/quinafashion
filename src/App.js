f5169f4be23bd56bab966eb0d81dc6b0e1f7d5ca
michael-mbugua <kamirimichael369@gmail.com>
2023-06-13 11:01:33 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Navbar from "./components/navbar/Navbar";
 import Homepage from "./components/homepage/Homepage";
 import Product from "./components/product/Product";
 import Sneakers from "./components/sneakers/Sneakers";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Details from "./components/item details/details";
+import ItemDetailsPage from "./components/item details page/ItemDetailsPage";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Homepage />
-      {/* <Product /> */}
-      <Sneakers />
-    </div>
+    <>
+      {/* <Navbar /> */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/fashions/:id" element={<ItemDetailsPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
